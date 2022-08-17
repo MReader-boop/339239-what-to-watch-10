95304ee0df31c1126b312230f1e9d32f42f8a6bd
MReader <mreader212@gmail.com>
2022-08-17 18:44:29 +0300

    Переносит работу со стейтом в компонент FilmList

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,34 +1,20 @@
-import React, {useState} from 'react';
-
 type FilmCardProps = {
   posterPreview: string;
   videoPreview: string;
   name: string;
+  onMouseEnter: React.MouseEventHandler<HTMLElement>;
+  onMouseLeave: React.MouseEventHandler<HTMLElement>;
 }
 
-function FilmCard({posterPreview, name, videoPreview}: FilmCardProps): JSX.Element {
-  const [isPreviewed, setIsPreviewed] = useState(false);
+function FilmCard({onMouseEnter, onMouseLeave, posterPreview, name, videoPreview}: FilmCardProps): JSX.Element {
   return(
-    <article className="small-film-card catalog__films-card"
-      onMouseEnter={() => setIsPreviewed(true)}
-      onMouseLeave={() => setIsPreviewed(false)}
-    >
-      {
-        isPreviewed ? (
-          <div className="small-film-card__image">
-            <img src={posterPreview} alt={name} width="280" height="175" />
-          </div>
-        ) : (
-          <React.Fragment>
-            <div className="small-film-card__image">
-              <img src={posterPreview} alt={name} width="280" height="175" />
-            </div>
-            <h3 className="small-film-card__title">
-              <a className="small-film-card__link" href="film-page.html">{name}</a>
-            </h3>
-          </React.Fragment>
-        )
-      }
+    <article className="small-film-card catalog__films-card" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      <div className="small-film-card__image">
+        <img src={posterPreview} alt={name} width="280" height="175" />
+      </div>
+      <h3 className="small-film-card__title">
+        <a className="small-film-card__link" href="film-page.html">{name}</a>
+      </h3>
     </article>
   );
 }
--- project/src/components/film-list/film-list.tsx
+++ project/src/components/film-list/film-list.tsx
@@ -1,18 +1,22 @@
 
 import {Film} from '../../types/film';
 import FilmCard from '../film-card/film-card';
+import {useState} from 'react';
 
 type FilmListProps = {
   films: Film[];
 };
 
 function FilmList({films}: FilmListProps): JSX.Element {
+  const [, setActiveFilmPreview] = useState('');
   return (
     <div className="catalog__films-list">
       {films.map(({card}, i) => {
         const key = `${i} - ${card.name}`;
         return (
           <FilmCard
+            onMouseEnter={()=>{setActiveFilmPreview(key);}}
+            onMouseLeave={()=>{setActiveFilmPreview('');}}
             videoPreview={card.videoPreview}
             posterPreview={card.posterPreview}
             name={card.name}
