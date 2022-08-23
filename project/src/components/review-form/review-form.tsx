6c7e60bc70556b0ac2a0052d21912e9a3577eb02
MReader <mreader212@gmail.com>
2022-08-23 09:21:25 +0300

    Исправляет работу со стейтом в компоненте «Форма отправки комментария»

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -2,49 +2,56 @@ import {useState, ChangeEvent} from 'react';
 
 function ReviewForm(): JSX.Element {
 
-  const [, setRating] = useState(0);
+  const textFieldChangeHandler = (evt: ChangeEvent<HTMLTextAreaElement>) => {
+    const value = evt.target.value;
+    setReviewText(value);
+  };
+
+  const ratingChangeHandler = (evt: ChangeEvent<HTMLInputElement>) => {
+    setRating(Number(evt.target.value));
+  };
+
+  const [rating, setRating] = useState(0);
+  const [reviewText, setReviewText] = useState('');
 
   return(
     <form action="#" className="add-review__form">
       <div className="rating">
-        <div className="rating__stars" onChange={({target}: ChangeEvent<HTMLInputElement>)=>{
-          setRating(Number(target.value));
-        }}
-        >
-          <input className="rating__input" id="star-10" type="radio" name="rating" value="10" />
+        <div className="rating__stars" onChange={ratingChangeHandler}>
+          <input className="rating__input" id="star-10" type="radio" name="rating" value="10" checked = {rating === 10}/>
           <label className="rating__label" htmlFor="star-10">Rating 10</label>
 
-          <input className="rating__input" id="star-9" type="radio" name="rating" value="9" />
+          <input className="rating__input" id="star-9" type="radio" name="rating" value="9" checked = {rating === 9}/>
           <label className="rating__label" htmlFor="star-9">Rating 9</label>
 
-          <input className="rating__input" id="star-8" type="radio" name="rating" value="8" />
+          <input className="rating__input" id="star-8" type="radio" name="rating" value="8" checked = {rating === 8}/>
           <label className="rating__label" htmlFor="star-8">Rating 8</label>
 
-          <input className="rating__input" id="star-7" type="radio" name="rating" value="7" />
+          <input className="rating__input" id="star-7" type="radio" name="rating" value="7" checked = {rating === 7}/>
           <label className="rating__label" htmlFor="star-7">Rating 7</label>
 
-          <input className="rating__input" id="star-6" type="radio" name="rating" value="6" />
+          <input className="rating__input" id="star-6" type="radio" name="rating" value="6" checked = {rating === 6}/>
           <label className="rating__label" htmlFor="star-6">Rating 6</label>
 
-          <input className="rating__input" id="star-5" type="radio" name="rating" value="5" />
+          <input className="rating__input" id="star-5" type="radio" name="rating" value="5" checked = {rating === 5}/>
           <label className="rating__label" htmlFor="star-5">Rating 5</label>
 
-          <input className="rating__input" id="star-4" type="radio" name="rating" value="4" />
+          <input className="rating__input" id="star-4" type="radio" name="rating" value="4" checked = {rating === 4}/>
           <label className="rating__label" htmlFor="star-4">Rating 4</label>
 
-          <input className="rating__input" id="star-3" type="radio" name="rating" value="3" />
+          <input className="rating__input" id="star-3" type="radio" name="rating" value="3" checked = {rating === 3}/>
           <label className="rating__label" htmlFor="star-3">Rating 3</label>
 
-          <input className="rating__input" id="star-2" type="radio" name="rating" value="2" />
+          <input className="rating__input" id="star-2" type="radio" name="rating" value="2" checked = {rating === 2}/>
           <label className="rating__label" htmlFor="star-2">Rating 2</label>
 
-          <input className="rating__input" id="star-1" type="radio" name="rating" value="1" />
+          <input className="rating__input" id="star-1" type="radio" name="rating" value="1" checked = {rating === 1}/>
           <label className="rating__label" htmlFor="star-1">Rating 1</label>
         </div>
       </div>
 
       <div className="add-review__text">
-        <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+        <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text" value={reviewText} onChange={textFieldChangeHandler}></textarea>
         <div className="add-review__submit">
           <button className="add-review__btn" type="submit">Post</button>
         </div>
